469094b43df6a06c87622296c78e627a3c5b8df3
Luis Bueno <lu.cusco@gmail.com>
2022-01-30 21:08:43 -0300

    Changind entry point and adding routes

--- public/index.php
+++ public/index.php
@@ -5,14 +5,41 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 # Load envs
 require SRC_DIR . 'config/loadEnvs.php';
 
-use App\Mail\Classes\SendEmail;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
-# Basic Test
-# TODO: Frontend
-$email = new SendEmail;
-$email->to('lc_bueno@hotmail.com', 'Louis');
-$email->content('Dev Test', "<h3>Uhull!</h3><p><b>Testing</b></p>");
-$email->send();
-var_dump($email,
-		$email->errorMessage);
+try {
+    // Load routes
+    $fileLocator = new FileLocator(array(SRC_DIR . '/Http/'));
+    $loader = new YamlFileLoader($fileLocator);
+    $routes = $loader->load('routes.yaml');
 
+    // Init RequestContext object
+    $context = new RequestContext();
+    $request = Request::createFromGlobals();
+    $context->fromRequest($request);
+
+    // Init UrlMatcher object
+    $matcher = new UrlMatcher($routes, $context);
+
+    // Find the current route
+    $parameters = $matcher->match($context->getPathInfo());
+
+    // Call 
+    list($controller, $method) = explode('::', $parameters['_controller']);
+    call_user_func(array($controller, $method), $request);
+
+} catch (ResourceNotFoundException $e) {
+    echo $e->getMessage();
+    http_response_code(404);
+} catch (MethodNotAllowedException $e) {
+    echo 'Method Not Allowed';
+    http_response_code(405);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
